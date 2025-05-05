3c1cf1d8a68ec4f8169c2f8c5e4cc08d120b039a
dinee10 <168616127+dinee10@users.noreply.github.com>
2025-05-05 15:17:49 +0530

    modify app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './pages/Home/Register';
-import Login from './pages/Home/Login';
-import Home from './pages/new/Home';
-import UserProfile from './pages/UserProfile/UserProfile';
-import UpdateProfile from './pages/UserProfile/UpdateProfile';
-import Progress from './pages/Progress/AddProgress';
-import ProgressList from './pages/Progress/Progress';
-import ProgressUpdate from './pages/Progress/ProgressUpdate';
-import Dashboard from './pages/Dasboard/Dashboard';
+import AppRoutes from './routes/Approutes'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,19 +7,7 @@ function App() {
   return (
     <>
       <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login/>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/profile' element={<UserProfile />} />
-          <Route path='/updateprofile/:id' element={<UpdateProfile />} />
-          <Route path='/addprogress' element={<Progress />} />
-          <Route path='/progresslist' element={<ProgressList />} />
-          <Route path='/update/:id' element={<ProgressUpdate />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          </Routes>
-          </BrowserRouter>
+      <AppRoutes />
       </div>
    
     </>
