0fee5b9a533e1cfa917143db435b04874d86e441
dinee10 <168616127+dinee10@users.noreply.github.com>
2025-05-06 19:37:15 +0530

    modeify the repository

--- server/src/main/java/server/repository/LprogressRepository.java
+++ server/src/main/java/server/repository/LprogressRepository.java
@@ -2,6 +2,10 @@ package server.repository;
 
 import server.model.LprogressModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import server.model.UserModel;
+
+import java.util.List;
 
 public interface LprogressRepository extends JpaRepository<LprogressModel, Long> {
+    List<LprogressModel> findByUser(UserModel user);
 }
