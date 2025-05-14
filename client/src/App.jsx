2802c69b6749bb60b92c841ffb6dbc80ceb141dd
Tharushi Wickramasinghe <it22132796@my.sliit.lk>
2025-05-14 14:53:09 +0530

    App Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Post from './pages/AddPost';
 import PostList from './pages/Post';
 import PostUpdate from './pages/PostUpdate';
+import ListPost from './pages/Postlist'
+import Individualpost from './pages/individualpost';
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
           <Route path='/addpost' element={<Post />} />
           <Route path='/postlist' element={<PostList />} />
           <Route path='/update/:id' element={<PostUpdate />} />
+          <Route path='/listpost' element={<ListPost />} />
+          <Route path='/post/:id'element= {<Individualpost/>}/>
           </Routes>
           </BrowserRouter>
       </div>
