8a559dda3287f99da6834f63e7354b707c459c0d
Tharushi Wickramasinghe <it22132796@my.sliit.lk>
2025-05-14 22:41:47 +0530

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server : {
-    port: 5173,
+    port: 5175,
     open: true,
   },
 })
