2a55b2b873b85fff322be3cd27321938b3fc20de
dinee10 <168616127+dinee10@users.noreply.github.com>
2025-05-15 01:08:30 +0530

    update app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,3 @@
-
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Post from './pages/AddPost';
-import PostList from './pages/Post';
-import PostUpdate from './pages/PostUpdate';
-import ListPost from './pages/Postlist'
-import Individualpost from './pages/individualpost';
-
-
-
-
 import AppRoutes from './routes/Approutes'
 
 function App() {
@@ -21,16 +9,7 @@ function App() {
       <div>
   
       <AppRoutes />
-   <BrowserRouter>
-        <Routes>
-          <Route path='/post' element={<Post />} />
-          <Route path='/addpost' element={<Post />} />
-          <Route path='/postlist' element={<PostList />} />
-          <Route path='/update/:id' element={<PostUpdate />} />
-          <Route path='/listpost' element={<ListPost />} />
-          <Route path='/post/:id'element= {<Individualpost/>}/>
-          </Routes>
-          </BrowserRouter>
+  
       </div>
    
 
