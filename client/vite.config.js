da3b44337dee053a576ab961c67b747f0307dff1
dinee10 <168616127+dinee10@users.noreply.github.com>
2025-05-15 01:07:39 +0530

    modify config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server : {
-    port: 5175,
+    port: 5173,
     open: true,
   },
 })
