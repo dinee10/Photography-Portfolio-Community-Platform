ad02ac3ab8b7cc42bc8dcd888ce1cbf697355190
dinee10 <168616127+dinee10@users.noreply.github.com>
2025-05-15 13:03:03 +0530

    modify blog repository class

--- server/src/main/java/server/repository/BlogRepository.java
+++ server/src/main/java/server/repository/BlogRepository.java
@@ -1,8 +1,11 @@
 package server.repository;
 
 import server.model.BlogModel;
+import server.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface BlogRepository extends JpaRepository <BlogModel, Long>{
+public interface BlogRepository extends JpaRepository<BlogModel, Long> {
+    List<BlogModel> findByUser(UserModel user);
 }
